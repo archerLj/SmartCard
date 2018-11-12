c0f615931912bddad31a423e75c78cb477281c59
archerLj <archerlj@TTdeMacBook-Pro.mshome.net>
2018-11-12 17:03:26 +0800

    init

---
+++ SmartCard/SmartCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCard
+//
+//  Created by archerLj on 2018/11/12.
+//  Copyright © 2018 NoOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
